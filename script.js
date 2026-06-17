fc5691d8f856a5dc1807ba0f567c82ac0aa7dcc0
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-06-18 00:33:04 +0530

    Add solution for step 4

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+const resizeButton = document.querySelector("#resize-btn");
 const container = document.querySelector(".container")
 
-for(let i = 0; i < 256; i++){
-    const item = document.createElement("div");
-    item.classList.add("items");
+let n = 16;
 
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "#2ecc71";
-    });
+createGrid(n);
+
+resizeButton.addEventListener("click", () => {
+    n = parseInt(prompt("Enter the new grid number: "));
+    
+    while(n > 100 || n < 0){
+        n = parseInt(prompt("Enter the new grid number: "));
+    }
+
+    createGrid(n);
+});
 
 
-    container.appendChild(item); 
+function createGrid(gridSize){
+    container.innerHTML = "";
+
+    const totalSquare = gridSize * gridSize;
+
+    for(let i = 0; i < totalSquare; i++){
+        const item = document.createElement("div");
+        item.classList.add("items");
+
+        const squareSize = 100 / gridSize;
+        item.style.flex = `0 0 ${squareSize}%`;
+        item.style.height = `${squareSize}%`; 
+
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "#2ecc71";
+        });
+
+
+        container.appendChild(item); 
+    }
 }
+
